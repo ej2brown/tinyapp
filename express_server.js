1427e19bc8244410c7f89409893309e099a60d61
Elizabeth Brown <ej2brown@edu.uwaterloo.ca>
2020-03-18 23:23:01 +0000

    created delete feature, modularized with routes/index

--- express_server.js
+++ express_server.js
@@ -1,76 +1,28 @@
-
-const express = require("express");
-const app = express();
-const PORT = 8080;  
-
 const bodyParser = require("body-parser");
+const express = require("express");
 
-app.use(bodyParser.urlencoded({extended: true}));
+//server
+const PORT = 8080;
+const app = express();
+
+
+//modularize routes
+const indexRouter = require("./routes/index");
+
+
+//setup middlewares and frameworks as the callback fn 
 app.set("view engine", "ejs");
 
-app.post("/urls", (req, res) => {
-  let longURL = req.body.longURL;
-  let shortURL = generateRandomString();
-urlDatabase[shortURL] = longURL;
-  res.send("Ok");        
-});
 
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+app.use("/", indexRouter);
 
 
-//to see a JSON string representing the entire urlDatabase object
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-  res.render("urls_show", templateVars);
-});
-
-function generateRandomString() {
-	const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-	const string_length = 6;
-	let randomstring = '';
-	for (let i=0; i<string_length; i++) {
-		let rnum = Math.floor(Math.random() * chars.length);
-		randomstring += chars.substring(rnum,rnum+1);
-	}
-return urlDatabase.randomstring = '';
-}
-
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = "";
-  for (let shortURL in urlDatabase) {
-    if (shortURL === req.params.shortURL) {
-      longUrl = urlDatabase[shortURL];
-    }
-  }
-  res.redirect(longURL);
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const router = express.Router();
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+//routes HTTP GET requests --> homepage
+router.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+
+router.post("/urls", (req, res) => {
+  let longURL = req.body; 
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.send("Ok");
+});
+
+/*routes Gets Requests */
+router.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+router.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+router.post("/urls/new", (req, res) => {
+  let longURL = req.body; 
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;  
+  const longURL = (longURL)
+});
+
+router.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
+router.get("/u/:shortURL", (req, res) => {
+  const longURL = "";
+  for (let shortURL in urlDatabase) {
+    if (shortURL === req.params.shortURL) {
+      longUrl = urlDatabase[shortURL];
+    }
+  }
+  res.redirect("urls_show");
+});
+
+router.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+})
+
+
+function generateRandomString() {
+  const chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  const string_length = 6;
+  let randomstring = '';
+  for (let i = 0; i < string_length; i++) {
+    let rnum = Math.floor(Math.random() * chars.length);
+    randomstring += chars.substring(rnum, rnum + 1);
+  }
+  return urlDatabase.randomstring = '';
+}
+
+/*practice 
+
+to see a JSON string representing the entire urlDatabase object
+
+router.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+router.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+}); */
+
+module.exports = router;
