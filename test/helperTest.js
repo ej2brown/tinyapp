b10dd3318ce8418c538eadf6dd90e899dd238aae
Elizabeth Brown <ej2brown@edu.uwaterloo.ca>
2020-03-31 00:29:27 +0000

    changed bug

--- test/helperTest.js
+++ test/helperTest.js
@@ -3,26 +3,26 @@ const { assert } = require('chai');
 const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
-  "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
-    password: "purple-monkey-dinosaur"
+  'userRandomID': {
+    id: 'userRandomID', 
+    email: 'user@example.com', 
+    password: 'purple-monkey-dinosaur'
   },
-  "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
-    password: "dishwasher-funk"
+  'user2RandomID': {
+    id: 'user2RandomID', 
+    email: 'user2@example.com', 
+    password: 'dishwasher-funk'
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
-    const expectedOutput = "userRandomID";
+    const user = getUserByEmail('user@example.com', testUsers)
+    const expectedOutput = 'userRandomID';
     assert.equal(user, expectedOutput);
   });
   it('should return indefined with an invalid email', function() {
-    const user = getUserByEmail("null@example.com", testUsers)
+    const user = getUserByEmail('null@example.com', testUsers)
     const expectedOutput = undefined;
     assert.equal(user, expectedOutput);
   });
